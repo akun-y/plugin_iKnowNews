19b73a15475c7e3860c275234cde32e2e419591e
akun <win11@bzly.home>
2023-11-16 00:23:45 +0800

    fix: help

--- inews.py
+++ inews.py
@@ -49,7 +49,7 @@ class iNews(Plugin):
         if e_context["context"].type == ContextType.PATPAT:
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+            e_context["context"].content = f"请你随机使用一种风格介绍你自己"
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
